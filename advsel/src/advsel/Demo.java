23845b5336c83cb56425e85d9b5cbeb1dfe0d8d9
KUMAR <KUMAR@DESKTOP-CIN16BI>
2022-12-27 15:18:16 +0530

    101

---
+++ advsel/src/advsel/Demo.java
@@ -0,0 +1,18 @@
+package advsel;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+@Test
+public void Test1()
+{
+	Reporter.log("HELLO",true);
+}
+@Test
+public void Test2()
+{
+	Reporter.log("HI",true);
+}
+}
